97e927e788f5be1c3dd3f640c589e4b6ad7ccb72
SilasYoome <71079352+SilasYoome@users.noreply.github.com>
2021-05-21 17:02:00 +0800

    add ch3

--- 03_expression/operators.java
+++ 03_expression/operators.java
@@ -24,7 +24,32 @@ public class operators{
         System.out.println("c" + c + "時");
         answer = b*b-4*a*c;
         System.out.println("b^2-4ac =" + answer);
+        
+        System.out.println("=========================");
+        System.out.println("比較運算子");
 
+        int x2 = 10,y2 = 20;
+        boolean a2 =true, b2=false;
 
+        System.out.print("x=" + x2);
+        System.out.println(" y=" + y2);
+        System.out.println("-------------------------");
+        System.out.println("x==y ==> " + (x2==y2));
+        System.out.println("x!=y ==> " + (x2!=y2));
+        System.out.println("x>y ==> " + (x2>y2));
+        System.out.println("x<y ==> " + (x2<y2));
+        System.out.println("x>=y ==> " + (x2>=y2));
+        System.out.println("x<=y ==> " + (x2<=y2));
+        System.out.println("=========================");
+        System.out.println("邏輯運算子");
+        System.out.print("a=" + a2);
+        System.out.println(" b=" + b2);
+        System.out.println("-------------------------");
+        System.out.println("not a   ==> " + !a2);
+        System.out.println("a and b     ==> " + (a2 && b2));
+        System.out.println("a or b     ==> " + (a2 || b2));
+        System.out.println("a nand b     ==> " + !(a2 && b2));
+        System.out.println("a nor b     ==> " + !(a2 || b2));
+        System.out.println("a and (x<y)     ==> " + (a2 && (x2<y2)));
     }
 }
